0c51e525519c40184f167b9a16e98c1fa3da449f
AnaKnis <anajulia.knis@gmail.com>
2021-01-26 11:15:42 -0300

    Primeiro Commit

---
+++ lib/Home.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  String _preco = "0";
+
+  void _recuperarPreco() async {
+    String url = "https://blockchain.info/ticker";
+    http.Response response = await http.get(url);
+    Map<String, dynamic> retorno = json.decode(response.body);
+    setState(() {
+      _preco = retorno["BRL"]["buy"].toString();
+    });
+    print("Resultado: " + retorno["BRL"]["buy"].toString());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+          padding: EdgeInsets.all(30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset("images/bitcoin.png"),
+              Padding(
+                padding: EdgeInsets.all(35),
+                child: Text("R\$" + _preco,
+                  style: TextStyle(
+                    fontSize: 35,
+                  ),
+                ),
+              ),
+              RaisedButton(
+                  child: Text ("Calcular", style:
+                  TextStyle( fontSize: 20, color: Colors.white,
+                  ),
+                  ),
+                  color: Colors.orange,
+                  padding: EdgeInsets.fromLTRB(30, 15, 30, 15),
+                  onPressed: _recuperarPreco)
+            ],
+          ),
+        )
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:app_bitcoin/Home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+
+
